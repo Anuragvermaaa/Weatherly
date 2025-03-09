05fce86628f3d199207816552a109e56e367c3fb
Anurag Verma <109055459+vermaanurag2002@users.noreply.github.com>
2025-03-09 15:41:53 +0530

    Add Files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Searchbox from './Searchbox'
+import InfoBox from './InfoBox';
+import './App.css'
+import WeatherApp from './WeatherApp';
+
+function App() {
+ 
+
+  return (
+    <>
+     
+    <WeatherApp />
+     
+
+    </>
+  )
+}
+
+export default App;
